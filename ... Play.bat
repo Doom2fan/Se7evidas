32547c0d35c52e1fd76d127921b9bcbccb8764d0
Chronos Ouroboros <doom2.fan@gmail.com>
2014-12-13 05:36:19 -0200

    Updated ... Play.bat

--- ... Play.bat
+++ ... Play.bat
@@ -1,6 +1,12 @@
 @ECHO OFF
 SETLOCAL
 
+SET PROG_ACC="./Tools/ACC/acc.EXE"
+SET ACSSOURCE="./ACS Source"
+SET ACS_DEST="./PK3 Source/acs"
+
+%PROG_ACC% %ACSSOURCE%/Se7evidas.ACS %ACS_DEST%/Se7evidas.O
+
 SET PROG_GZDOOM="../../Final Duel/dis.EXE"
 
 %PROG_GZDOOM% -file "%CD%/PK3 Source" -file %1 -file %2 -file %3 -file %4 -file  %5 -file %6
