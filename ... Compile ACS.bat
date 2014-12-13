4af3f989eb519fbca6e1070b4893ed9d1b9f3df7
Chronos Ouroboros <doom2.fan@gmail.com>
2014-12-13 06:03:20 -0200

    AAAAAAAAAAAAUUUUUUUUUUUUGH. Fixed ACS compilation scripts.

--- ... Compile ACS.bat
+++ ... Compile ACS.bat
@@ -5,6 +5,8 @@ SET PROG_ACC="./Tools/ACC/acc.EXE"
 SET ACSSOURCE="./ACS Source"
 SET ACS_DEST="./PK3 Source/acs"
 
+DEL "./PK3 Source/acs"
+MKDIR "./PK3 Source/acs"
 %PROG_ACC% %ACSSOURCE%/Se7evidas.ACS %ACS_DEST%/Se7evidas.O
 
 PAUSE
