ce70ac3496fb2da2ded482ccebd2496cd8bb6d0a
Chronos Ouroboros <doom2.fan@gmail.com>
2015-01-20 11:28:23 -0200

    Bleh.

--- ... Play - GZDoom.bat
+++ ... Play - GZDoom.bat
@@ -9,6 +9,6 @@ RMDIR /S /Q "./PK3 Source/acs"
 MKDIR "./PK3 Source/acs"
 %PROG_ACC% %ACSSOURCE%/Se7evidas.ACS %ACS_DEST%/Se7evidas.O
 
-SET PROG_GZDOOM="../../Final Duel/dis.EXE"
+SET PROG_GZDOOM="../../gzdoom.EXE"
 
 %PROG_GZDOOM% -file "%CD%/PK3 Source" -file %1 -file %2 -file %3 -file %4 -file  %5 -file %6
