f30bdda3bf4216d21bdbf687aa59bc7a3efb81dc
Chronos Ouroboros <doom2.fan@gmail.com>
2015-08-21 09:50:04 -0300

    No idea 1 - .BAT file

--- ... Play - GZDoom.bat
+++ ... Play - GZDoom.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 SETLOCAL
 
-SET PROG_GZDOOM="%DOOMDIR%\... Older versions\GZDoom\Latest 1.9\gzdoom.EXE"
+SET PROG_GZDOOM="%DOOMDIR%\. GZDoom\gzdoom.EXE"
 
 %PROG_GZDOOM% -file "%CD%/PK3 Source" -file "%CD%/PK3_Source_GZDoom" %*
