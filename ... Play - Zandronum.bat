d56646d497cc13bb99579d09ab8f9bddfefa3cfe
Chronos Ouroboros <doom2.fan@gmail.com>
2015-12-12 11:57:38 -0200

    Hurr.

--- ... Play - Zandronum.bat
+++ ... Play - Zandronum.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 SETLOCAL
 
-SET PROG_ZANDRONUM="C:\Users\Chronos Ouroboros\Games\Doom\... Other ports\Zandronum 3.x\zandronum.EXE"
+SET PROG_ZANDRONUM="%DOOMDIR%\Zandronum\zandronum.EXE"
 
 %PROG_ZANDRONUM% -stdout "%CD%/PK3 Source" "%CD%/PK3_Source_Zandronum" %*
