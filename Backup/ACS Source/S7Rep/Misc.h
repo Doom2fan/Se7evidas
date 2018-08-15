784c54c7e7d9a8691b879196297665f7303b2b37
Chronos Ouroboros <doom2.fan@gmail.com>
2018-08-15 11:30:34 -0300

    Removed the ACS/C source code backup. Not very useful anymore.

--- Backup/ACS Source/S7Rep/Misc.h
+++
@@ -1,7 +0,0 @@
-script "S7_GetEffectsStayTime" (void) {
-    setResultValue (0);
-}
-
-script "S7_GetEffectsFadeSpeed" (void) {
-    setResultValue (0);
-}
--- Backup/ACS Source/Zandronum/S7Rep/Misc.h
+++
@@ -1,9 +0,0 @@
-script "S7_GetEffectsStayTime" (void) NET CLIENTSIDE {
-    int result = getUserCVar (CPLN, "S7_EffectStayTime");
-    setResultValue (result);
-}
-
-script "S7_GetEffectsFadeSpeed" (void) NET CLIENTSIDE {
-    int result = getUserCVar (CPLN, "S7_EffectFadeSpeed");
-    setResultValue (result);
-}
