d24c4b00ff4fcfbe546fb39dcc2568ac105e4e7c
Chronos Ouroboros <doom2.fan@gmail.com>
2024-11-15 01:27:00 -0300

    Remove QZDoom from the test batch file.

--- !!Test.bat
+++ !!Test.bat
@@ -4,9 +4,6 @@ SETLOCAL
 if "%S7PROG_GZDOOM%" == "" (
     SET S7PROG_GZDOOM="gzdoom"
 )
-if "%S7PROG_QZDOOM%" == "" (
-    SET S7PROG_QZDOOM="qzdoom"
-)
 
 SET PROG_PROG=""
 SET PROG_ARGS=""
@@ -15,20 +12,16 @@ if %ERRORLEVEL% NEQ 0 (
     GOTO :Finish
 )
 
-ECHO 1 - GZDoom
-ECHO 2 - QZDoom
-ECHO 0 - Exit
-CHOICE /C 120 /N
+rem ECHO 1 - GZDoom
+rem ECHO 0 - Exit
+rem CHOICE /C 10 /N
 
-if %ERRORLEVEL% EQU 3 (
-    goto :Finish
-) else if %ERRORLEVEL% EQU 2 (
-    SET PROG_PROG=%S7PROG_QZDOOM%
-    SET PROG_ARGS=-stdout -file "%CD%/PK3 Source" %S7_QZDOOMARGS% %*
-) else if %ERRORLEVEL% EQU 1 (
+rem if %ERRORLEVEL% EQU 2 (
+rem     goto :Finish
+rem ) else if %ERRORLEVEL% EQU 1 (
     SET PROG_PROG=%S7PROG_GZDOOM%
     SET PROG_ARGS=-stdout -file "%CD%/PK3 Source" %S7_GZDOOMARGS% %*
-)
+rem )
 
 "%PROG_PROG%" %PROG_ARGS%
 
